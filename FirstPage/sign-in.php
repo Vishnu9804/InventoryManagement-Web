0bdc7384e85a4ad48d2117aa87f877b4bcb42cc0
Vishnu9804 <145148109+Vishnu9804@users.noreply.github.com>
2024-02-10 16:48:32 +0530

    Update sign-in.php

--- FirstPage/sign-in.php
+++ FirstPage/sign-in.php
@@ -98,7 +98,7 @@ if (isset($_POST['signup'])) {
                   </div>
                   <p class="mt-4 text-sm text-center">
                     Don't have an account?
-                    <a href="../pages/sign-up.php" class="text-primary text-gradient font-weight-bold">Sign up</a>
+                    <a href="../FirstPage/sign-up.php" class="text-primary text-gradient font-weight-bold">Sign up</a>
                   </p>
                 </form>
               </div>
